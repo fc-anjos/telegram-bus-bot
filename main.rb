a733766b2b3b3820033af27df766dc9ba38b5195
fc-anjos <felipe.anjos@usp.br>
2020-03-27 19:48:15 -0300

    example test from telegram-bot-ruby documentation

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
+require 'rubygems'
+require 'telegram/bot'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+    when '/end'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+    end
   end
 end
