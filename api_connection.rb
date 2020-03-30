71adf5ccbfc1afc3d6735cfe0b3f1aab7fc28e53
fc-anjos <felipe.anjos@usp.br>
2020-03-30 15:15:58 -0300

    connect front and back ends

--- api_connection.rb
+++ api_connection.rb
@@ -12,11 +12,3 @@ class Connection
     result.body
   end
 end
-
-connection = Connection.new
-result = connection.search('8000')
-puts result
-# agent = Mechanize.new
-# agent.post('http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token=41a811d106147fd42bb8205ed6ff75a19b3ace1d79564d2ec522cad6fc930f41')
-# page = agent.get 'http://api.olhovivo.sptrans.com.br/v2.1/Linha/Buscar?termosBusca=8000'
-# puts page.body
