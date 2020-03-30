6e2c94b5db3003186ade69d95f7934e1bcd2dc8c
fc-anjos <felipe.anjos@usp.br>
2020-03-30 16:27:57 -0300

    refactor search into lines search

--- api_connection.rb
+++ api_connection.rb
@@ -7,8 +7,8 @@ class Connection
     @agent.post('http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token=41a811d106147fd42bb8205ed6ff75a19b3ace1d79564d2ec522cad6fc930f41')
   end
 
-  def search(term)
-    result = @agent.get "http://api.olhovivo.sptrans.com.br/v2.1/Linha/Buscar?termosBusca=#{term}"
-    result.body
+  def lines(term)
+    lines = @agent.get "http://api.olhovivo.sptrans.com.br/v2.1/Linha/Buscar?termosBusca=#{term}"
+    lines.body
   end
 end
