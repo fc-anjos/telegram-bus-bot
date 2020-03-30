211aadfde5de9fdd49873e08454261c108936182
fc-anjos <felipe.anjos@usp.br>
2020-03-30 15:30:28 -0300

    connect front-end and back end

--- main.rb
+++ main.rb
@@ -8,9 +8,9 @@ Telegram::Bot::Client.run(token) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Fala,  #{message.from.first_name}! Vou fazer um robô pra te mandar um alôzinho então, demorou??")
-    # when '/8000'
-      # search_result = Connection.new.search('8000')
-      # bot.api.send_message(chat_id: message.chat.id, text: search_result.to_s)
+    when '/8000'
+      search_result = Connection.new.search('8000')
+      bot.api.send_message(chat_id: message.chat.id, text: search_result.to_s)
     when '/end'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
     else
