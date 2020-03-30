ee7e210badb2c93ec49a3a95b35855f98f292160
fc-anjos <felipe.anjos@usp.br>
2020-03-30 19:47:33 -0300

    better format to answers

--- lib/display.rb
+++ lib/display.rb
@@ -2,6 +2,7 @@ class Display
   def format_signs(signs)
     string = ''
     signs.each_with_index do |(_code, sign), index|
+      string += "\n" if index >= 0
       string += "#{index + 1}:\n#{sign}\n"
     end
     string
