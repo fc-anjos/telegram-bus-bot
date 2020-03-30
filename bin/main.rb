8ea50b79170f9753480b3887bf8cc752be4a95d3
fc-anjos <felipe.anjos@usp.br>
2020-03-30 19:24:59 -0300

    include main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -4,6 +4,7 @@ require_relative '../lib/api_connection.rb'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
 Telegram::Bot::Client.run(token) do |bot|
+  connection = Connection.new
   bot.listen do |message|
     case message.text
     when '/start'
@@ -11,10 +12,9 @@ Telegram::Bot::Client.run(token) do |bot|
                            'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
 
     else
-      connection = Connection.new
       lines = connection.lines(message)
-      connection.get_signs(lines)
-      connection.signs.each do |_code, sign|
+      signs = connection.get_signs(lines)
+      signs.each do |_code, sign|
         bot.api.send_message(chat_id: message.chat.id, text: sign)
       end
     end
