cfb0d4138d02c17eb8e4cd3d5217a802d187ba1a
fc-anjos <felipe.anjos@usp.br>
2020-03-31 11:25:26 -0300

    Make back-end queries reflect development done on front-end

--- lib/api_connection.rb
+++ lib/api_connection.rb
@@ -20,42 +20,27 @@ class Connection
   def parse_lines(lines_body)
     line_xml = Nokogiri::HTML(lines_body, nil, Encoding::UTF_8.to_s)
     line_json = JSON.parse(line_xml.css('body p').inner_html)
-    # Returns an array of the found lines
     line_json
   end
 
   def stops_per_line(line_code)
     stops = @agent.get "#{@base_url}/Parada/BuscarParadasPorLinha?codigoLinha=#{line_code}"
+    stops
   end
 
   def get_signs(lines)
-    @signs = {}
+    # TODO: Make it work for circular lines
+    signs = {}
     lines.each do |line|
       line_code = line['cl']
       sign_number = line['lt'].to_s + '-' + line['tl'].to_s
       if line['sl'] == 1
-        sign = "#{sign_number} #{line['tp']} => #{line['ts']}"
+        sign = "#{sign_number}\n#{line['tp']} => #{line['ts']}"
       elsif line['sl'] == 2
-        sign = "#{sign_number} #{line['ts']} => #{line['tp']}"
+        sign = "#{sign_number}\n#{line['ts']} => #{line['tp']}"
       end
-      @signs[line_code] = sign
+      signs[line_code] = sign
     end
+    signs
   end
 end
-
-class Display
-  def initialize(signs)
-    @signs = signs
-  end
-
-  def format_signs
-    @signs.each do |_code, sign|
-      puts sign
-    end
-  end
-end
-
-connection = Connection.new
-lines = connection.lines('8000')
-connection.get_signs(lines)
-Display.new(connection.signs).format_signs
