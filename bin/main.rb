23aa1202cba94e8558fa829066f89c0c06ada936
fc-anjos <felipe.anjos@usp.br>
2020-03-31 12:43:22 -0300

    move get_signs to Display module

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,22 @@
 require 'rubygems'
 require 'telegram/bot'
-require_relative '../lib/api_connection'
-require_relative '../lib/display'
+# require_relative '../lib/api_connection'
+# require_relative '../lib/display'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
 Telegram::Bot::Client.run(token) do |bot|
-  connection = Connection.new
+  # connection = Connection.new
   bot.listen do |message|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Fala,  #{message.from.first_name}!" \
                            'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
 
-    else
-      lines = connection.lines(message)
-      signs = connection.get_signs(lines)
-      answer = Display.new.format_signs(signs)
-      bot.api.send_message(chat_id: message.chat.id, text: answer)
+      # else
+      # lines = connection.lines(message)
+      # signs = connection.get_signs(lines)
+      # answer = Display.new.format_signs(signs)
+      # bot.api.send_message(chat_id: message.chat.id, text: answer)
     end
   end
 end
