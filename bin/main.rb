eb3b4882b1792212471bf94c9abeb75d51353118
fc-anjos <felipe.anjos@usp.br>
2020-03-31 18:14:11 -0300

    wrap first question in a method

--- bin/main.rb
+++ bin/main.rb
@@ -5,9 +5,16 @@ require_relative '../lib/api_connection'
 
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
+def which_bus(message)
+  lines = @connection.lines(message)
+  signs = @display.get_signs(lines)
+  signs = @display.format_signs(signs)
+  bot.api.send_message(chat_id: message.chat.id, text: signs)
+end
+
 Telegram::Bot::Client.run(token) do |bot|
-  connection = Connection.new
-  display = Display.new
+  @connection = Connection.new
+  @display = Display.new
 
   bot.listen do |message|
     case message.text
@@ -16,13 +23,10 @@ Telegram::Bot::Client.run(token) do |bot|
                            'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
 
     else
-      lines = connection.lines(message)
-      signs = display.get_signs(lines)
-      signs = display.format_signs(signs)
-      bot.api.send_message(chat_id: message.chat.id, text: signs)
+      which_bus(message)
 
       bot.listen do |message2|
-        options = display.prepare_selection(lines)
+        options = @display.prepare_selection(lines)
         chosen = options[message2.text.to_i]
         bot.api.send_message(chat_id: message.chat.id, text: chosen)
       end
