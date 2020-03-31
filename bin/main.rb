14e1578202c3f6b5b5de84b674359912abf3fe61
fc-anjos <felipe.anjos@usp.br>
2020-03-31 12:48:29 -0300

    telegram sanity check

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,7 @@ require 'telegram/bot'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
 Telegram::Bot::Client.run(token) do |bot|
-  connection = Connection.new
+  # connection = Connection.new
   bot.listen do |message|
     case message.text
     when '/start'
