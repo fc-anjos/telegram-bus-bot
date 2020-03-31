2450c956957d08aeaf152cdd316d2a9ad71fddc7
fc-anjos <felipe.anjos@usp.br>
2020-03-31 18:45:44 -0300

    ask for an answer on the first method

--- bin/main.rb
+++ bin/main.rb
@@ -5,12 +5,18 @@ require_relative '../lib/api_connection'
 
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
-def select_lines(message, bot, connection, display)
-  lines = connection.lines(message)
-  signs = display.get_signs(lines)
-  signs = display.format_signs(signs)
-  bot.api.send_message(chat_id: message.chat.id, text: signs)
-end
+# def select_lines(message, bot, connection, display)
+#   lines = connection.lines(message)
+#   signs = display.get_signs(lines)
+#   signs = display.format_signs(signs)
+#   bot.api.send_message(chat_id: message.chat.id, text: signs)
+
+#   bot.listen do |message2|
+#     options = display.prepare_selection(lines)
+#     chosen = options[(message2.text.to_i - 1)]
+#     bot.api.send_message(chat_id: message.chat.id, text: chosen)
+#   end
+# end
 
 Telegram::Bot::Client.run(token) do |bot|
   connection = Connection.new
@@ -20,21 +26,14 @@ Telegram::Bot::Client.run(token) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Fala,  #{message.from.first_name}!" \
-                           'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
-
-    #     else
-    #       bot.api.send_message(chat_id: message.chat.id, text: "Foi mal,  #{message.from.first_name}!" \
-    #    'você quebrou o robozinho')
+                             'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
 
     else
-      select_lines(message, bot, connection, display)
+      bot.api.send_message(chat_id: message.chat.id, text: "Foi mal,  #{message.from.first_name}!" \
+   'você quebrou o robozinho')
 
-      bot.listen do |message2|
-        options = display.prepare_selection(lines)
-        chosen = options[(message2.text.to_i - 1)]
-        bot.api.send_message(chat_id: message.chat.id, text: chosen)
-      end
-
+      # else
+      # select_lines(message, bot, connection, display)
     end
   end
 end
