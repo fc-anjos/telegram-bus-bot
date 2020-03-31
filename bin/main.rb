86b97cf9f00a1dc1e40ec028fff2bfacd77b790e
fc-anjos <felipe.anjos@usp.br>
2020-03-31 11:06:44 -0300

    better format strings

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 require 'telegram/bot'
-# require_relative '../lib/api_connection'
-# require_relative '../lib/display'
+require_relative '../lib/api_connection'
+require_relative '../lib/display'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
+# connection = Connection.new
+# lines = connection.lines('8000')
+# signs = connection.get_signs(lines)
+# answer = Display.new.format_signs(signs)
+# puts answer
+
 Telegram::Bot::Client.run(token) do |bot|
-  # connection = Connection.new
+  connection = Connection.new
   bot.listen do |message|
     case message.text
     when '/start'
@@ -13,10 +19,10 @@ Telegram::Bot::Client.run(token) do |bot|
                            'Vou fazer um robô pra te mandar um alôzinho então, demorou??')
 
     else
-      # lines = connection.lines(message)
-      # signs = connection.get_signs(lines)
-      # answer = Display.new.format_signs(signs)
-      # bot.api.send_message(chat_id: message.chat.id, text: answer)
+      lines = connection.lines(message)
+      signs = connection.get_signs(lines)
+      answer = Display.new.format_signs(signs)
+      bot.api.send_message(chat_id: message.chat.id, text: answer)
     end
   end
 end
