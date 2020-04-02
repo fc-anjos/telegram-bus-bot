8cc9cea829f5f3c8a0a6af421488abde879202b2
fc-anjos <felipe.anjos@usp.br>
2020-04-02 14:12:22 -0300

    change connection and display to class_instance variable

--- bin/main.rb
+++ bin/main.rb
@@ -4,20 +4,20 @@ require_relative '../lib/display'
 require_relative '../lib/api_connection'
 token = '1007984866:AAHy5tUA-a_Vo5U8KTxKpLbB1SkZ-FZJX_E'
 
-def show_lines(message, bot, connection, display)
-  lines = connection.lines(message)
-  signs = display.get_signs(lines)
-  signs = display.format_message(signs)
+def show_lines(message, bot)
+  lines = @connection.lines(message)
+  signs = @display.get_signs(lines)
+  signs = @display.format_message(signs)
   bot.api.send_message(chat_id: message.chat.id, text: 'Please select your line by typing an option number')
   bot.api.send_message(chat_id: message.chat.id, text: 'Options:')
   bot.api.send_message(chat_id: message.chat.id, text: signs)
-  chosen = select_lines(bot, display, lines)
+  chosen = select_lines(bot, lines)
   chosen
 end
 
-def select_lines(bot, display, lines)
+def select_lines(bot, lines)
   bot.listen do |message|
-    options = display.prepare_selection(lines)
+    options = @display.prepare_selection(lines)
     choice = nil
     until choice
       begin
@@ -33,21 +33,21 @@ def select_lines(bot, display, lines)
   end
 end
 
-def show_stops(message, line_code, bot, connection, display)
-  stops_hash = connection.stops_per_line(line_code)
-  stops = display.get_stops(stops_hash)
-  stops = display.format_message(stops)
+def show_stops(message, line_code, bot)
+  stops_hash = @connection.stops_per_line(line_code)
+  stops = @display.get_stops(stops_hash)
+  stops = @display.format_message(stops)
 
   bot.api.send_message(chat_id: message.chat.id, text: stops)
 
   bot.listen do |message_stop|
     chosen = nil
-    return select_stop(message_stop, bot, display, stops_hash) until chosen
+    return select_stop(message_stop, bot, stops_hash) until chosen
   end
 end
 
-def select_stop(message, bot, display, stops_hash)
-  options = display.prepare_selection(stops_hash)
+def select_stop(message, bot, stops_hash)
+  options = @display.prepare_selection(stops_hash)
   choice = message.text.to_i
   return options[choice - 1] if choice <= options.length && choice.positive?
 
@@ -55,20 +55,20 @@ def select_stop(message, bot, display, stops_hash)
   false
 end
 
-def message_arrival_time(connection, message, bot, display, stop_code, line_code)
-  hash_arrivals = connection.estimate_arrival(stop_code, line_code)
-  arrivals_list = display.get_arrivals(hash_arrivals)
+def message_arrival_time(message, bot, stop_code, line_code)
+  hash_arrivals = @connection.estimate_arrival(stop_code, line_code)
+  arrivals_list = @display.get_arrivals(hash_arrivals)
   if arrivals_list.empty?
     bot.api.send_message(chat_id: message.chat.id, text: 'There are no expected arrivals for this line at this stop!')
   else
-    format_arrivals = display.format_arrivals(arrivals_list)
+    format_arrivals = @display.format_arrivals(arrivals_list)
     bot.api.send_message(chat_id: message.chat.id, text: "Your bus should arrive at #{format_arrivals}")
   end
 end
 
 Telegram::Bot::Client.run(token) do |bot|
-  connection = Connection.new
-  display = Display.new
+  @connection = Connection.new
+  @display = Display.new
 
   bot.listen do |message|
     case message.text
@@ -81,11 +81,11 @@ Telegram::Bot::Client.run(token) do |bot|
 
     else
       line_code = nil
-      line_code = show_lines(message, bot, connection, display) until line_code
+      line_code = show_lines(message, bot) until line_code
       bot.api.send_message(chat_id: message.chat.id, text: 'Please select your stop by typing an option number')
       bot.api.send_message(chat_id: message.chat.id, text: 'Options:')
-      stop_code = show_stops(message, line_code, bot, connection, display)
-      message_arrival_time(connection, message, bot, display, stop_code, line_code)
+      stop_code = show_stops(message, line_code, bot)
+      message_arrival_time(message, bot, stop_code, line_code)
     end
   end
 end
